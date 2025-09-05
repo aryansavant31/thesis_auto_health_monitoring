89bb3d72dc6dfc10f0118cc1bae0404418340e8d
aryan_asml_vdi <aryansavant31@gmail.com>
2025-09-05 15:01:41 +0200

    fdet - tswp2 and iswp1 with new tb tags

--- data/config.py
+++ data/config.py
@@ -55,18 +55,18 @@ class DataConfig:
                                 'ASM':'asml',
                                 'ASMT':'asml_trial'}
         
-        self.application = 'MSD'
-        self.machine_type = 'M004'
-        self.scenario = 'scene_1'
+        self.application = 'ASM'
+        self.machine_type = 'NXE'
+        self.scenario = 'full_wafer'
 
-        self.signal_types = MSDGroupMaker().m004_all
+        self.signal_types = NXEGroupMaker().ammf_acc
         
         self.fs = None #np.array([[48000]])    # sampling frequency matrix, set in the data.prep.py
         self.format = 'hdf5'  # options: hdf5
 
         # segement data
-        self.window_length      = 100
-        self.stride             = 100
+        self.window_length      = 1000
+        self.stride             = 1000
 
         self.use_custom_max_timesteps = False
         self.max_timesteps     = 10000
@@ -89,8 +89,8 @@ class DataConfig:
 
         self.healthy_configs   = {
             #'0_N': [get_augment_config('OG')],
-            'series_tp': [get_augment_config('OG')]  
-            #key: [get_augment_config('OG')] for key in self.view.healthy_types[:50] if key.startswith('E1')
+            #'series_tp': [get_augment_config('OG')]  
+            key: [get_augment_config('OG')] for key in self.view.healthy_types[:50] if key.startswith('E1')
         }
         
         self.unhealthy_configs = {
